d942af289c528a324c09e239bfada2bffda395d8
luc <luc.leray@gmail.com>
2020-07-27 20:01:18 +0200

    add max paths

--- pages/[color].js
+++ pages/[color].js
@@ -1,4 +1,4 @@
-export { getStaticPaths, getStaticProps } from '../lib/get-named-colors-paths'
+export { getStaticPaths, getStaticProps } from '../lib/get-hex-colors-max-paths'
 
 export default function ColorPage({ color }) {
   return <div style={{ width: '100%', height: '100vh', background: color }} />
