3abdd26ea78ee60b9811558df7d9a60308879f4e
luc <luc.leray@gmail.com>
2021-03-12 20:52:13 +0100

    refactor

--- pages/index.js
+++ pages/index.js
@@ -1,3 +1,17 @@
+import Link from 'next/link'
+import { useReducer } from 'react'
+
+import { getRandomColor } from '../lib/get-hex-colors-paths'
+
 export default function IndexPage() {
-  return 'hello'
+  const [color, shuffle] = useReducer(getRandomColor, null, getRandomColor)
+
+  return (
+    <div>
+      <Link href={`/${color}`}>
+        <a>#{color}</a>
+      </Link>{' '}
+      - <button onClick={shuffle}>Shuffle</button>
+    </div>
+  )
 }
